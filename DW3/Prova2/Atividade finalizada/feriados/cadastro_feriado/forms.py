d1ea91bcc806cbda2c3bd10465c1eed9eef578ee
JpedroBH <51427296+JpedroBH@users.noreply.github.com>
2022-06-15 19:30:23 -0300

    Delete DW3/Prova2/Atividade finalizada directory

--- DW3/Prova2/Atividade finalizada/feriados/cadastro_feriado/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from cadastro_api.models import FeriadoModel
-
-class FeriadoForm(ModelForm):
-
-    class Meta:
-        
-        model = FeriadoModel
-        fields = ['nome', 'data']
